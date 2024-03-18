f3c2a349bc0a30b0eaa06f86b4dc20c1caba34e9
aarifsafavi <aarifsafavi@gmail.com>
2024-03-18 23:37:29 +0800

    remove

--- src/Components/News/News.js
+++ src/Components/News/News.js
@@ -53,7 +53,7 @@ const News = () => {
 
   useEffect(() => {
     fetchData();
-  }, []);
+  });
 
   const loadMoreArticles = () => {
     const newIndex = displayedNews.length;
@@ -150,7 +150,7 @@ const News = () => {
                     {ele.author === null ? "Unknown" : ele.author}
                   </h5>
                   <p className="card-text">{ele.title}</p>
-                  <a href={ele.url} target="_blank" className="btn btn-primary">
+                  <a href={ele.url} className="btn btn-primary">
                     Read More
                   </a>
                   <input
